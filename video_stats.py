af454d82180407ba8423a2d1376768c3875df0f1
Suresh Lama <sonamhyolmo.345@gmail.com>
2026-03-15 21:32:29 +1300

    data extration using api wrappedup

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from datetime import date 
 from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
@@ -94,8 +95,16 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+# extract data to the data file
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
 
